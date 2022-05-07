7b03822a7795dbd8c4ffc62f876c65f10db695d2
yukki <72681477+yukki2288@users.noreply.github.com>
2022-05-07 23:09:19 +0900

    #8 helpコマンドを追加

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ client.on("interactionCreate", async (interaction) => {
     });
   }
 
+  // スラッシュコマンド
   if (!interaction.isCommand) return;
   const cmdName = interaction.commandName;
   const manager = connectionManagers.get(interaction.guildId);
@@ -364,6 +365,51 @@ client.on("interactionCreate", async (interaction) => {
       });
     });
   }
+
+  // ヘルプ
+  if (cmdName === "help") {
+    interaction.reply({
+      embeds: [{
+        title: "ヘルプ",
+        color: "43a1ec",
+        timestamp: new Date(),
+        fields: [
+          {
+            name: "`/join`",
+            value: "読み上げを開始します"
+          },
+          {
+            name: "`/leave`",
+            value: "読み上げを終了します"
+          },
+          {
+            name: "`/word`",
+            value: "単語を辞書に登録します。「読み」が空欄の場合は単語を削除します（サーバーごと）"
+          },
+          {
+            name: "`/speaker`",
+            value: "自分のメッセージを読み上げる話者を設定します（サーバーごと）"
+          },
+          {
+            name: "`/settings`",
+            value: "「read_name」は名前の読み上げを、「join_left_check」は入退出の読み上げと通知を、設定します（サーバーごと）"
+          },
+          {
+            name: "`/dictionary export`",
+            value: "辞書を出力します"
+          },
+          {
+            name: "`/dictionary delete`",
+            value: "辞書を削除します"
+          },
+          {
+            name: "`「メッセージのメニュー」→「アプリ」→「辞書として追加」`",
+            value: "辞書をインポートします"
+          }
+        ]
+      }]
+    });
+  }
 });
 
 
@@ -484,6 +530,10 @@ function setCommands(message) {
     {
       name: "leave",
       description: "読み上げを終了します"
+    },
+    {
+      name: "help",
+      description: "ヘルプを表示します"
     },
     {
       name: "speaker",
