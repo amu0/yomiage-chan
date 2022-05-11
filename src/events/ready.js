894b68ee6b648c6195845de4fe04632c272b4362
yukki <72681477+yukki2288@users.noreply.github.com>
2022-05-12 01:31:10 +0900

    起動時にコマンドを登録するように変更

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,19 @@
+const fs = require("fs");
+
 module.exports = {
   name: "ready",
   once: true,
-  execute(client) {
+  async execute(client) {
     console.log(`${client.user.tag}でログインしています。`);
+
+    const interactions = [];
+    const interactionFiles = fs.readdirSync("./src/interactions").filter(file => file.endsWith(".js"));
+
+    for (const file of interactionFiles) {
+      const interaction = require(`../interactions/${file}`);
+      interactions.push(interaction.data.toJSON());
+    }
+    await client.application.commands.set(interactions);
+    console.log("コマンドをセットしました");
   }
 }
